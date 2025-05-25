e4e18c1f5171af3dd66d19770ddce4c9226ed30c
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-05-25 22:23:33 +0900

    iframeで動画埋め込み

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,180 @@
 export default function Home() {
   return (
     <>
-      <div
-        className="tagembed-widget"
-        // style="width:100%;height:100%"
-        data-widget-id="2172173"
-        data-tags="false"
-        view-url="https://widget.tagembed.com/2172173"
-      ></div>
-      <script src="https://widget.tagembed.com/embed.min.js" type="text/javascript"></script>
+      <div className="flex">
+        <blockquote
+          className="tiktok-embed"
+          cite="https://www.tiktok.com/@le_sserafim/video/7490910405871602952"
+          data-video-id="7490910405871602952"
+          // style={{ maxWidth: 605, minWidth: 325 }}
+        >
+          <section>
+            <a
+              target="_blank"
+              title="@le_sserafim"
+              href="https://www.tiktok.com/@le_sserafim?refer=embed"
+            >
+              @le_sserafim
+            </a>
+            <a
+              title="bangjeemin"
+              target="_blank"
+              href="https://www.tiktok.com/tag/bangjeemin?refer=embed"
+            >
+              #BANGJEEMIN
+            </a>
+            is burning 𝓗𝓞𝓣🧲🐱
+            <a
+              title="le_sserafim"
+              target="_blank"
+              href="https://www.tiktok.com/tag/le_sserafim?refer=embed"
+            >
+              #LE_SSERAFIM
+            </a>
+            <a
+              title="르세라핌"
+              target="_blank"
+              href="https://www.tiktok.com/tag/%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C?refer=embed"
+            >
+              #르세라핌
+            </a>
+            <a
+              title="kimchaewon"
+              target="_blank"
+              href="https://www.tiktok.com/tag/kimchaewon?refer=embed"
+            >
+              #KIMCHAEWON
+            </a>
+            <a
+              title="김채원"
+              target="_blank"
+              href="https://www.tiktok.com/tag/%EA%B9%80%EC%B1%84%EC%9B%90?refer=embed"
+            >
+              #김채원
+            </a>
+            <a
+              title="le_sserafim_hot"
+              target="_blank"
+              href="https://www.tiktok.com/tag/le_sserafim_hot?refer=embed"
+            >
+              #LE_SSERAFIM_HOT
+            </a>
+            ❤️‍🔥
+            <a title="izna" target="_blank" href="https://www.tiktok.com/tag/izna?refer=embed">
+              #izna
+            </a>
+            <a
+              title="이즈나"
+              target="_blank"
+              href="https://www.tiktok.com/tag/%EC%9D%B4%EC%A6%88%EB%82%98?refer=embed"
+            >
+              #이즈나
+            </a>
+            <a
+              title="방지민"
+              target="_blank"
+              href="https://www.tiktok.com/tag/%EB%B0%A9%EC%A7%80%EB%AF%BC?refer=embed"
+            >
+              #방지민
+            </a>
+            @izna (이즈나)
+            <a
+              target="_blank"
+              title="♬ HOT - LE SSERAFIM"
+              href="https://www.tiktok.com/music/HOT-7478650503288457232?refer=embed"
+            >
+              ♬ HOT - LE SSERAFIM
+            </a>
+          </section>
+        </blockquote>
+        <script async src="https://www.tiktok.com/embed.js"></script>
+        <blockquote
+          className="tiktok-embed"
+          cite="https://www.tiktok.com/@le_sserafim/video/7490910405871602952"
+          data-video-id="7490910405871602952"
+          // style={{ maxWidth: 605, minWidth: 325 }}
+        >
+          <section>
+            <a
+              target="_blank"
+              title="@le_sserafim"
+              href="https://www.tiktok.com/@le_sserafim?refer=embed"
+            >
+              @le_sserafim
+            </a>
+            <a
+              title="bangjeemin"
+              target="_blank"
+              href="https://www.tiktok.com/tag/bangjeemin?refer=embed"
+            >
+              #BANGJEEMIN
+            </a>
+            is burning 𝓗𝓞𝓣🧲🐱
+            <a
+              title="le_sserafim"
+              target="_blank"
+              href="https://www.tiktok.com/tag/le_sserafim?refer=embed"
+            >
+              #LE_SSERAFIM
+            </a>
+            <a
+              title="르세라핌"
+              target="_blank"
+              href="https://www.tiktok.com/tag/%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C?refer=embed"
+            >
+              #르세라핌
+            </a>
+            <a
+              title="kimchaewon"
+              target="_blank"
+              href="https://www.tiktok.com/tag/kimchaewon?refer=embed"
+            >
+              #KIMCHAEWON
+            </a>
+            <a
+              title="김채원"
+              target="_blank"
+              href="https://www.tiktok.com/tag/%EA%B9%80%EC%B1%84%EC%9B%90?refer=embed"
+            >
+              #김채원
+            </a>
+            <a
+              title="le_sserafim_hot"
+              target="_blank"
+              href="https://www.tiktok.com/tag/le_sserafim_hot?refer=embed"
+            >
+              #LE_SSERAFIM_HOT
+            </a>
+            ❤️‍🔥
+            <a title="izna" target="_blank" href="https://www.tiktok.com/tag/izna?refer=embed">
+              #izna
+            </a>
+            <a
+              title="이즈나"
+              target="_blank"
+              href="https://www.tiktok.com/tag/%EC%9D%B4%EC%A6%88%EB%82%98?refer=embed"
+            >
+              #이즈나
+            </a>
+            <a
+              title="방지민"
+              target="_blank"
+              href="https://www.tiktok.com/tag/%EB%B0%A9%EC%A7%80%EB%AF%BC?refer=embed"
+            >
+              #방지민
+            </a>
+            @izna (이즈나)
+            <a
+              target="_blank"
+              title="♬ HOT - LE SSERAFIM"
+              href="https://www.tiktok.com/music/HOT-7478650503288457232?refer=embed"
+            >
+              ♬ HOT - LE SSERAFIM
+            </a>
+          </section>
+        </blockquote>
+        <script async src="https://www.tiktok.com/embed.js"></script>
+      </div>
     </>
   );
 }
